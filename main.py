c3af835366e5ad1ee0f418b998424da2b81636a5
taichihub <taichikuniv21@gmail.com>
2024-07-15 18:09:47 +0900

    機能修正・リファクタリング

--- main.py
+++ main.py
@@ -3,50 +3,59 @@ import logging
 import pandas as pd
 from yt_dlp import YoutubeDL
 from tqdm import tqdm
-from settings import INPUT_LINK_DATA, OUTPUT_FORMAT, OUTPUT_DIRECTORY
+from settings import INPUT_LINK_DATA, OUTPUT_DIRECTORY, ERROR_DIRECTORY, OUTPUT_FORMAT, YDL_OPTS_STATIC, ERROR_TYPES, HEADER1, HEADER2, HEADER3
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger()
 
 def download_youtube_video_as_audio(url, output_directory, output_format, output_filename):
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'outtmpl': os.path.join(output_directory, f'{output_filename}.%(ext)s'),
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': output_format,
-            'preferredquality': '192',
-        }],
-        'quiet': True,
-        'no_warnings': True,
-    }
+    ydl_opts = YDL_OPTS_STATIC.copy()
+    ydl_opts['outtmpl'] = os.path.join(output_directory, f'{output_filename}.%(ext)s')
+    ydl_opts['postprocessors'][0]['preferredcodec'] = output_format
+    ydl_opts['logger'] = logger
+
     try:
         with YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
-        logger.info(f"ダウンロードと変換完了: {output_filename}.{output_format}")
+        return None, None, None
     except Exception as e:
-        logger.error(f"{url} のダウンロード中にエラーが発生しました: {e}")
+        error_type = ERROR_TYPES["other"]
+        if "format not available" in str(e):
+            error_type = ERROR_TYPES["unsupported_url"]
+        elif "ERROR:" in str(e):
+            error_type = ERROR_TYPES["invalid_url"]
+        logger.error(f"ダウンロード中に問題が発生しました URL:{url}, Error: {str(e)}")
+        return url, output_filename, error_type
+
+def save_error_log(errors):
+    if errors:
+        df_errors = pd.DataFrame(errors, columns=[HEADER1, HEADER2, HEADER3])
+        os.makedirs(os.path.dirname(ERROR_DIRECTORY), exist_ok=True)
+        df_errors.to_csv(ERROR_DIRECTORY, index=False)
+        logger.info(f"エラーログを保存しました: {ERROR_DIRECTORY}")
 
 def batch_download_youtube_videos_as_audio(video_info_list, output_directory, output_format):
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
     
+    error_logs = []
     total_videos = len(video_info_list)
-    with tqdm(total=total_videos, desc="進捗状況", disable=True) as pbar:
-        for i, video_info in enumerate(video_info_list, start=1):
+    with tqdm(total=total_videos, desc="進捗状況") as pbar:
+        for video_info in video_info_list:
             try:
-                url = video_info['YouTubeリンク']
-                filename = video_info['出力ファイル名']
-                download_youtube_video_as_audio(url, output_directory, output_format, filename)
+                url = video_info[HEADER1]
+                filename = video_info[HEADER2]
+                error_url, error_filename, error_type = download_youtube_video_as_audio(url, output_directory, output_format, filename)
+                if error_url:
+                    error_logs.append({HEADER1: error_url, HEADER2: error_filename, HEADER3: error_type})
             except KeyError as e:
                 logger.error(f"キーエラー: {e}。スキップします。ビデオ情報: {video_info}")
-                continue
             except Exception as e:
                 logger.error(f"予期しないエラーが発生しました: {e}。スキップします。ビデオ情報: {video_info}")
-                continue
-            pbar.update(1)
-            progress = (i / total_videos) * 100
-            tqdm.write(f"進捗状況: {progress:.2f}%")
+            finally:
+                pbar.update(1)
+
+    save_error_log(error_logs)
 
 if __name__ == "__main__":
     video_info_list = pd.read_csv(INPUT_LINK_DATA).to_dict('records')
--- settings.py
+++ settings.py
@@ -5,7 +5,38 @@ now = datetime.now()
 OUTPUT_FORMAT = 'mp3'
 
 # 入力リンクデータ
-INPUT_LINK_DATA = 'data/music_list.csv'
+INPUT_LINK_DATA = 'data/video_info_list.csv'
 
 # 出力先ディレクトリ
-OUTPUT_DIRECTORY = f'/Users/kawasakitakuma/Downloads/musics/{now.strftime("%Y%m%d%H%M")}_変換音声'
+OUTPUT_DIRECTORY = f'/Users/chiroru/Downloads/EDM/{now.strftime("%Y%m%d%H%M")}_変換音声'
+
+# エラーCSV出力ディレクトリ
+ERROR_DIRECTORY = f'data/errors/{now.strftime("%Y%m%d_%H%M")}_エラー.csv'
+
+# タイムアウト秒数
+SOCKET_TIMEOUT = 10
+
+# yt_dlpの設定
+YDL_OPTS_STATIC = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredquality': '192',
+    }],
+    'quiet': False,
+    'no_warnings': False,
+    'socket_timeout': SOCKET_TIMEOUT
+}
+
+# エラータイプ定数
+ERROR_TYPES = {
+    "unsupported_url": "URLがYouTubeでサポートされていない",
+    "invalid_url": "URLが有効なものではない",
+    "duplicate_filename": "全く同じ名前のファイルがある",
+    "other": "その他が原因のエラー"
+}
+
+# CSV ヘッダー定数
+HEADER1 = 'YouTubeリンク'
+HEADER2 = '出力ファイル名'
+HEADER3 = 'エラータイプ'
